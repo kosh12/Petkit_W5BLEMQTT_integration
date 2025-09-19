76c1d50a16f1ce95f7b7ca4efdb96783f82cf327
kosh12 <kosh12@gmail.com>
2025-09-19 10:26:18 +0300

    Update to const.py

---
+++ const.py
@@ -0,0 +1,11 @@
+# const.py
+DOMAIN = "petkit_w5_ble"
+PLATFORMS = ["sensor", "switch", "device_tracker"]
+
+CONF_DEVICES = "devices"
+CONF_MAC = "mac"
+CONF_NAME = "name"
+CONF_MQTT_TOPIC = "mqtt_topic"
+
+DEFAULT_NAME = "Petkit W5"
+DEFAULT_MQTT_TOPIC = "petkit/w5"
